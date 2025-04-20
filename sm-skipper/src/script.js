2045c220b6e3ec192e9aee8c83267bc08047aee6
Ethan Cheung <150068258+ItsAnimePotato@users.noreply.github.com>
2025-04-20 01:49:23 -0400

    got spotify queue to display song name, artist, if SM

---
+++ sm-skipper/src/script.js
@@ -0,0 +1,88 @@
+// Because this is a literal single page application
+// we detect a callback from Spotify by checking for the hash fragment
+import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
+import { urlList } from "./smList";
+
+const clientId = "205f326a865e4840ac5230dc7937a368";
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (!code) {
+  redirectToAuthCodeFlow(clientId);
+} else {
+  const accessToken = await getAccessToken(clientId, code);
+  const pProfile = await fetchProfile(accessToken);
+  populateUI(pProfile);
+  const qProfile = await fetchQueue(accessToken);
+  populateQueue(qProfile);
+  populateTable(qProfile);
+}
+
+async function fetchProfile(code) {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${code}` },
+  });
+
+  return await result.json();
+}
+
+function populateUI(profile) {
+  document.getElementById("displayName").innerText = profile.display_name;
+  document.getElementById("avatar").setAttribute("src", profile.images[0].url);
+  document.getElementById("id").innerText = profile.id;
+  document.getElementById("email").innerText = profile.email;
+  document.getElementById("uri").innerText = profile.uri;
+  document
+    .getElementById("uri")
+    .setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url").innerText = profile.href;
+  document.getElementById("url").setAttribute("href", profile.href);
+  document.getElementById("imgUrl").innerText = profile.images[0].url;
+  document.getElementById("uri").innerText = profile.uri;
+}
+
+async function fetchQueue(code) {
+  const result = await fetch("https://api.spotify.com/v1/me/player/queue", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${code}` },
+  });
+
+  return await result.json();
+}
+
+function populateQueue(profile) {
+  document.getElementById("currentSong").innerText =
+    profile.currently_playing.name;
+  document.getElementById("fullQueueLength").innerText = profile.queue.length;
+  document.getElementById("nextSong").innerText = profile.queue[0].name;
+}
+
+function populateTable(profile) {
+  let table = document.getElementById("tableBody");
+  for (let i = 0; i < profile.queue.length; i++) {
+    let song = profile.queue[i];
+    let songName = song.name;
+    let artistNames = song.artists.map((artist) => artist.name).join(", ");
+    let songOfSM = smSong(song);
+
+    let row = `<tr>
+      <td>${i + 1}</td>
+      <td>${songName}</td>
+      <td>${artistNames}</td>
+      <td>${songOfSM}</td>
+    </tr>`;
+    table.innerHTML += row;
+  }
+}
+
+function smSong(song) {
+  for (let i = 0; i < song.artists.length; i++) {
+    let artistURL = song.artists[i].external_urls.spotify;
+    return areUrlsEqual(artistURL);
+  }
+}
+
+function areUrlsEqual(artistURL) {
+  return urlList.some((url) => url === artistURL);
+}
---
+++ sm-skipper/src/smList.js
@@ -0,0 +1,33 @@
+export const urlList = [
+  //main??? groups
+  "https://open.spotify.com/artist/6YVMFz59CuY7ngCxTxjpxE",
+  "https://open.spotify.com/artist/4muJrGMndyYWqZtfk8OWy4",
+  "https://open.spotify.com/artist/0il5ZP3xYOECtONJtZ38Ln",
+  "https://open.spotify.com/artist/3cjEqqelV9zb4BYE3qDQ4O",
+  "https://open.spotify.com/artist/3wRA5UYoo08BBKJnzyKkpF",
+  "https://open.spotify.com/artist/0Sadg1vgvaPqGTOjxu0N6c",
+  "https://open.spotify.com/artist/5rGgflnIpRNizTCozbYBuY",
+  "https://open.spotify.com/artist/6XXKPxRX2WWPPtfodzpc2v",
+  //7 NCT
+  "https://open.spotify.com/artist/48eO052eSDcn8aTxiv6QaG",
+  "https://open.spotify.com/artist/7f4ignuCJhLXfZ9giKT7rH",
+  "https://open.spotify.com/artist/1gBUSTR3TyDdTVFIaQnc02",
+  "https://open.spotify.com/artist/4FqmqIspLaUGtxAFFLsZxc",
+  "https://open.spotify.com/artist/3paGCCtX1Xr4Gx53mSeZuQ",
+  "https://open.spotify.com/artist/1qBsABYUrxg9afpMtyoFKz",
+  "https://open.spotify.com/artist/0W0w607z3JEA1vXLz9FVGw",
+  "https://open.spotify.com/artist/1z4g3DjTBBZKhvAroFlhOM",
+  "https://open.spotify.com/artist/6bwp9ObI8FWvMPCIWVBmhl", //Red Velvet I + S
+  "https://open.spotify.com/artist/2jOm3cYujQx6o1dxuiuqaX",
+  "https://open.spotify.com/artist/2hRQKC0gqlZGPrmUKbcchR",
+  "https://open.spotify.com/artist/49VtaZvoqBgZHQxSqlCUyp", //SM Town
+  //5 Super Junior
+  "https://open.spotify.com/artist/6gzXCdfYfFe5XKhPKkYqxV",
+  "https://open.spotify.com/artist/4yNdRuMECm7sFnAOQ6MieS",
+  "https://open.spotify.com/artist/3EfnKIG9827OXkcQQT7HEI",
+  "https://open.spotify.com/artist/3POJzSnl0opkk0nLWBAAr8",
+  "https://open.spotify.com/artist/5Kdp1Iq26eCz7XbPxOgt7F",
+  "https://open.spotify.com/artist/5BHFSMEjfLVx1JwRWjAOsE",
+  "https://open.spotify.com/artist/13rF01aOogvnkuQXOlgTW8",
+  "https://open.spotify.com/artist/6nVMMEywS5Y4tsHPKx1nIo",
+];
